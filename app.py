a8a7a16b8bb74d3f33ea25bbd8ae514d5624a035
eraldo76 <eraldolattari@gmail.com>
2023-11-20 22:51:45 +0100

    app

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
 
 connect(
     host=f"mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@fnt-mongodb-b69449db.mongo.ondigitalocean.com/?authMechanism=DEFAULT")
-
+log
 login_manager = LoginManager(app)
 # Redirect to register view if user is not logged in
 login_manager.login_view = "login"
