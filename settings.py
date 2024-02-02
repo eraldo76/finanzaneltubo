3dcb5096c15ad50430bc6b62adceb538242ce039
Eraldo <eraldolattari@gmail.com>
2024-02-02 22:14:21 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,12 +1,12 @@
 # Configura la chiave segreta
-SECRET_KEY = 'JackandLeo14'
-YOUTUBE_API_KEY = "AIzaSyA5iN9hP6gydfkEiieQ_iXFLanWsRTM5Mk"
+SECRET_KEY = 'xxxx'
+YOUTUBE_API_KEY = "xxx"
 
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = 'sk-xxxxx'
 OPENAI_MODEL = 'gpt-4'
 
 
-PEXELS_API_KEY = 'azSlqa1jyPRpZ4zo6AMu2U6Q0u9N19qXMzn4XvbPc121T23aCSoMdUZN'
+PEXELS_API_KEY = 'xxxxx'
 PEXELS_BASE_URL = 'https://api.pexels.com/v1/search'
 
 PROTOCOLLO = "http://"
